22b2b3fc1a7a8294a30a046db495dcc826141334
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2023-08-18 13:12:25 +0330

    Implement product interface to product class

---
+++ ReportingTool/ReportingTool.Data/Repository/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using ReportingTool.Data.Repository.IRepository;
+using System.Linq;
+
+namespace ReportingTool.Data.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ReportingTool_DbEntities _db;
+
+        public ProductRepository(ReportingTool_DbEntities db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product product)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == product.Id);
+            objFromDb.ProductName = product.ProductName;
+            objFromDb.QuantityInStock = product.QuantityInStock;
+            objFromDb.Price = product.Price;
+        }
+    }
+}
