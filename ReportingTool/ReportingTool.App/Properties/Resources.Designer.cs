1f786e20ef5b213be6dad44d17fee98ebb902bfe
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2023-08-18 23:25:03 +0330

    Add app icon to resources

--- ReportingTool/ReportingTool.App/Properties/Resources.Designer.cs
+++ ReportingTool/ReportingTool.App/Properties/Resources.Designer.cs
@@ -119,5 +119,15 @@ namespace ReportingTool.App.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap warehouse_500px {
+            get {
+                object obj = ResourceManager.GetObject("warehouse_500px", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
