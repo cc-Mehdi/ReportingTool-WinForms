32e41fb82ee7f276bb78193895474bad3dcceb5d
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2023-08-18 13:14:01 +0330

    Create unit-of-work interface

---
+++ ReportingTool/ReportingTool.Data/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ReportingTool.Data.Repository.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IProductRepository productRepository { get; }
+        void Save();
+    }
+}
