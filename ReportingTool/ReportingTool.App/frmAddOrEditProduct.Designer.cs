8931604b89cbb1bb4f45b7eece92a8b8b5ec89f4
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2023-08-18 16:08:41 +0330

    Create add or edit product form

---
+++ ReportingTool/ReportingTool.App/frmAddOrEditProduct.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ReportingTool.App
+{
+    partial class frmAddOrEditProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.lblAddOrEditProduct = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numQuantityInStock = new System.Windows.Forms.NumericUpDown();
+            this.numPrice = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantityInStock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPrice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.Green;
+            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSubmit.Location = new System.Drawing.Point(430, 217);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(142, 32);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Add";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.Transparent;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBack.Location = new System.Drawing.Point(334, 217);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(90, 32);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            // 
+            // lblAddOrEditProduct
+            // 
+            this.lblAddOrEditProduct.AutoSize = true;
+            this.lblAddOrEditProduct.Font = new System.Drawing.Font("Verdana", 16F);
+            this.lblAddOrEditProduct.Location = new System.Drawing.Point(12, 6);
+            this.lblAddOrEditProduct.Name = "lblAddOrEditProduct";
+            this.lblAddOrEditProduct.Size = new System.Drawing.Size(146, 26);
+            this.lblAddOrEditProduct.TabIndex = 2;
+            this.lblAddOrEditProduct.Text = "Add Product";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(578, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "_________________________________________________________";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Product Name : ";
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(199, 59);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(373, 27);
+            this.txtProductName.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Quantity In Stock : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(119, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Price : ";
+            // 
+            // numQuantityInStock
+            // 
+            this.numQuantityInStock.Location = new System.Drawing.Point(199, 111);
+            this.numQuantityInStock.Maximum = new decimal(new int[] {
+            999999,
+            0,
+            0,
+            0});
+            this.numQuantityInStock.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numQuantityInStock.Name = "numQuantityInStock";
+            this.numQuantityInStock.Size = new System.Drawing.Size(373, 27);
+            this.numQuantityInStock.TabIndex = 8;
+            this.numQuantityInStock.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numPrice
+            // 
+            this.numPrice.Location = new System.Drawing.Point(199, 167);
+            this.numPrice.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.numPrice.Name = "numPrice";
+            this.numPrice.Size = new System.Drawing.Size(373, 27);
+            this.numPrice.TabIndex = 9;
+            // 
+            // frmAddOrEditProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(61)))), ((int)(((byte)(86)))));
+            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.numPrice);
+            this.Controls.Add(this.numQuantityInStock);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblAddOrEditProduct);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnSubmit);
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(234)))), ((int)(((byte)(252)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "frmAddOrEditProduct";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Add Product";
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantityInStock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPrice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label lblAddOrEditProduct;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numQuantityInStock;
+        private System.Windows.Forms.NumericUpDown numPrice;
+    }
+}
