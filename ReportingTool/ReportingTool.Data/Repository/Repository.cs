9e18e4904dd342d63565b7a2b043d5b6654e2d5b
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2023-08-18 13:08:15 +0330

    Implement IRepository interface to Repository class

---
+++ ReportingTool/ReportingTool.Data/Repository/Repository.cs
@@ -0,0 +1,45 @@
+﻿using ReportingTool.Data.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ReportingTool.Data.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ReportingTool_DbEntities _db;
+        internal DbSet<T> _dbSet;
+
+        public Repository(ReportingTool_DbEntities db)
+        {
+            _db = db;
+            _dbSet = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = _dbSet;
+            return query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null)
+        {
+            IQueryable<T> query = _dbSet;
+            if (filter != null)
+                query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
