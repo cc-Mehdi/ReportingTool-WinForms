78e0d15fda6d11303e46e39055d91acbf9f8557b
Mehdi Gholami <57840939+cc-Mehdi@users.noreply.github.com>
2023-08-18 13:16:13 +0330

    Implement unit-of-work interface

---
+++ ReportingTool/ReportingTool.Data/Repository/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using ReportingTool.Data.Repository.IRepository;
+
+namespace ReportingTool.Data.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ReportingTool_DbEntities _db;
+
+        public UnitOfWork(ReportingTool_DbEntities db)
+        {
+            _db = db;
+            productRepository = new ProductRepository(_db);
+        }
+
+        public IProductRepository productRepository { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
